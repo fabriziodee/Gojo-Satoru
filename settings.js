1d2ace0a20dd5bd386f79446cb0c00c7f20b3d23
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-27 23:06:21 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['393518398856']
+global.premium = ['393518398856']
+global.ownernomer = '393518398856'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
